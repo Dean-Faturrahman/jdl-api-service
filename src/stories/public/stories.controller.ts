20467193564e1ed62562e6e8974cda63e3ffe38f
diiin <dean12fatur@gmail.com>
2025-08-02 20:39:09 +0700

    modify add story translations

--- src/stories/public/stories.controller.ts
+++ src/stories/public/stories.controller.ts
@@ -4,6 +4,7 @@ import { UpdateStoryDto } from '../dto/update-story.dto';
 import { WebResponse } from 'src/model/web.model';
 import { StoriesService } from './stories.service';
 import { Public } from 'src/auth/decorator/public.decorator';
+import { LanguageQueryDto } from 'src/common/dto/language-query.dto';
 
 @Controller('api/v1/stories')
 export class StoriesController {
@@ -12,10 +13,11 @@ export class StoriesController {
   @Public()
   @Get()
   async findAll(
+    @Query() query: LanguageQueryDto,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
   ): Promise<WebResponse<any>> {
-    const result = await this.storiesService.findAll(page, limit);
+    const result = await this.storiesService.findAll(query, page, limit);
     return {
       status_code: HttpStatus.OK,
       message: 'Successfully retrieved all stories',
@@ -25,8 +27,11 @@ export class StoriesController {
 
   @Public()
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number): Promise<WebResponse<any>> {
-    const result = await this.storiesService.findOne(id);
+  async findOne(
+    @Query() query: LanguageQueryDto,
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<WebResponse<any>> {
+    const result = await this.storiesService.findOne(query, id);
     return {
       status_code: HttpStatus.OK,
       message: 'Successfully retrieved a story',
--- src/stories/public/stories.service.ts
+++ src/stories/public/stories.service.ts
@@ -3,23 +3,54 @@ import { CreateStoryDto } from '../dto/create-story.dto';
 import { UpdateStoryDto } from '../dto/update-story.dto';
 import { PrismaService } from 'src/common/prisma.service';
 import { I18nContext } from 'nestjs-i18n';
+import { LanguageQueryDto } from 'src/common/dto/language-query.dto';
 
 @Injectable()
 export class StoriesService {
   constructor(private readonly prisma: PrismaService) { }
 
-  async findAll(page: number = 1, limit: number = 10) {
+  async findAll(query: LanguageQueryDto, page: number = 1, limit: number = 10) {
+    const lang = I18nContext.current()?.lang;
     const skip = (page - 1) * limit;
 
-    const stories = await this.prisma.story.findMany({
-      skip: skip,
-      take: limit,
-      orderBy: {
-        id: 'desc'
-      }
-    })
+    const [stories, total] = await this.prisma.$transaction([
 
-    const total = await this.prisma.story.count()
+      this.prisma.story.findMany({
+        skip: skip,
+        take: limit,
+        include: {
+          translations: {
+            where: {
+              language_code: lang,
+            },
+          },
+        },
+        orderBy: {
+          id: 'desc',
+        },
+      }),
+      this.prisma.story.count(),
+    ]);
+
+    const formattedStories = stories.map(story => {
+      const translation = story.translations[0];
+      const content = translation?.content || null;
+      // Truncate content to 200 characters if available
+      const shortContent = content
+        ? content.length > 200
+          ? content.substring(0, 200) + '...'
+          : content
+        : null;
+
+      return {
+        id: story.id,
+        image_url: story.image_url,
+        title: translation?.title || null,
+        content: shortContent,
+        created_at: story.createdAt,
+        updated_at: story.updatedAt
+      };
+    });
 
     return {
       pagination: {
@@ -27,17 +58,40 @@ export class StoriesService {
         page: page,
         limit: limit,
       },
-      stories,
-    }
+      data: formattedStories,
+    };
   }
 
-  async findOne(id: number) {
-    const story = await this.prisma.story.findUnique({
+  async findOne(query: LanguageQueryDto, id: number) {
+    const lang = I18nContext.current()?.lang;
+
+    const storyRaw = await this.prisma.story.findUnique({
+      include: {
+        translations: {
+          select: {
+            language_code: true,
+            title: true,
+            content: true
+          },
+          where: {
+            language_code: lang
+          }
+        }
+      },
       where: { id },
     });
 
-    if (!story) {
+    if (!storyRaw) {
       throw new NotFoundException(`Story with ID ${id} not found`);
+    }
+
+    const story = {
+      id: storyRaw.id,
+      image_url: storyRaw.image_url,
+      title: storyRaw.translations[0]?.title || null,
+      content: storyRaw.translations[0]?.content || null,
+      created_at: storyRaw.createdAt,
+      updated_at: storyRaw.updatedAt
     }
 
     return story
